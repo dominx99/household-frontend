75e98591bb4620864d9bebb439cf11648cfdba47
dominx99 <dominx992@gmail.com>
2022-07-30 23:54:04 +0200

    Change color palette

--- src/app/mui/themes.ts
+++ src/app/mui/themes.ts
@@ -1,22 +1,17 @@
 import { createTheme } from "@mui/material";
-import { blueGrey, grey, red } from "@mui/material/colors";
+import { grey, lightBlue } from "@mui/material/colors";
 
 export const darkTheme = createTheme({
   palette: {
     mode: 'dark',
-    background: {
-      default: blueGrey[800],
-      paper: blueGrey[800]
-    },
     text: {
       primary: grey[100],
       secondary: grey[100],
       disabled: grey[100],
     },
     primary: {
-      light: blueGrey[500],
-      main: blueGrey[500],
-      contrastText: '#fefefe',
-    }
+      ...lightBlue,
+      contrastText: grey[200],
+    },
   },
 })
